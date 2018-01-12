e81704b89a5744516baf248bec9b11f37fa545da
Joshua Fogg <joshua.h.fogg@gmail.com>
2018-01-12 20:25:41 +0000

    Switched from ./ to /tmp and renamed to wribo.sh

--- wribo.sh
+++ wribo.sh
@@ -56,33 +56,33 @@ fi
 # PROMPT SELECTION
 
 # Download frontpage of r/WritingPrompts
-wget -q "https://www.reddit.com/r/WritingPrompts/top/.rss" -O page
+wget -q "https://www.reddit.com/r/WritingPrompts/top/.rss" -O /tmp/page
 
 # Change up file data
-sed -i "s/>/\n/g" page
-sed -i "s/<\/title//g" page
+sed -i "s/>/\n/g" /tmp/page
+sed -i "s/<\/title//g" /tmp/page
 
 # Iterate over lines in file to find prompts
-while [ -s page ]; do
+while [ -s /tmp/page ]; do
 
     # If first line is a writing prompt
-    if [[ "$(head -n 1 page)" == "[WP] "* ]]; then
+    if [[ "$(head -n 1 /tmp/page)" == "[WP] "* ]]; then
         
         # Print the prompt
-        prompt=$(head -n 1 page | sed -e "s/\[WP\]\ //g")
+        prompt=$(head -n 1 /tmp/page | sed -e "s/\[WP\]\ //g")
         echo -e "\n\nPrompt $((3-skips))"
         echo -e "--------"
         echo -e "$prompt\n"
 
         if [[ $skips == 0 ]]; then
             read -r -p "Ran out of skips, you must use this prompt!"
-            echo -e "Prompt: $prompt\n\n" >> doc
-            rm page
+	    echo -e "Prompt $((3-skips)): $prompt\n\n" >> /tmp/doc
+            rm /tmp/page
         else
             while true; do
                 read -r -p "Do you want to use this prompt? " answer
                 case $answer in
-                    [Yy]* ) echo -e "Prompt: $prompt\n\n" >> doc; rm page; break;;
+                    [Yy]* ) echo -e "Prompt $((3-skips)): $prompt\n\n" >> /tmp/doc; rm /tmp/page; break;;
                     [Nn]* ) skips=$((skips-1)); break;;
                     * ) echo "Please answer [Y]es or [N]o.";;
                 esac
@@ -91,8 +91,8 @@ while [ -s page ]; do
     fi
     
     # Remove line from file
-    touch page
-    sed -i '1d' page
+    touch /tmp/page
+    sed -i '1d' /tmp/page
 done
 
 
@@ -100,25 +100,25 @@ done
 
 while true; do
     # Open doc in vim
-    vim doc +3 -c ":set syntax=markdown"
+    vim /tmp/doc +3 -c ":set wrap"
     
     # Calculate wordcount
-    cp doc prose
-    sed -i '1,2d' prose
-    wordcount=$(wc -w prose | sed -e "s/\ prose//")
-    
+    cp /tmp/doc /tmp/prose
+    sed -i '1,2d' /tmp/prose
+    wordcount=$(wc -w /tmp/prose | sed -e "s/\ \/tmp\/prose//")
+    rm /tmp/prose
+
     if [ "$wordcount" -lt "$wordgoal" ]; then
         read -r -p "Not enough words! Need $((500-wordcount)) more."
     else
         echo "Writing complete!"
         break
     fi
-    rm prose
 done
 
 
 # CLEAN UP
 
-aspell check doc
-mv -i doc "$savefold$(date +"%Y-%m-%d.md")"
-rm page prose .doc.swp doc.bak 2> /dev/null 
+aspell check /tmp/doc
+mv -i /tmp/doc "$savefold$(date +"%Y-%m-%d.md")"
+rm /tmp/page /tmp/prose /tmp/.doc.swp /tmp/doc.bak 2> /dev/null
